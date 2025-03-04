856e3fef8d64ca927622a3e1431a31058131739f
restuprtma <restu.prtma05@gmail.com>
2025-03-04 09:05:19 +0700

    Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,40 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [
-                'username'=>'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username'=>'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
+                'level_id' => 2,
+            ]
         );
+
+        $user->username = 'manager12';
+
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama', 'username']));
+        // $user->isDirty();
+        // $user->isDirty('username');
+        // $user->isDirty('nama');
+        // $user->isDirty(['nama','username']);
+
+        // $user->isClean();
+        // $user->isClean('username');
+        // $user->isClean('nama');
+        // $user->isClean(['nama','username']);
+
+        // $user->save();
+
+        // $user->isDirty();
+        // $user->isClean();
+        // dd($user->isDirty());
+        // return view('user', ['data' => $user]);
 
 
         // $data = [
