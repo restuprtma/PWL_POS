68ca8ba2e39b8c9331d895277f55e7a320fe1d19
restuprtma <restu.prtma05@gmail.com>
2025-03-11 21:54:27 +0700

    Implementasikan web layout dan datatables pada table Kategori

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -123,7 +123,7 @@ class KategoriController extends Controller
     public function update(Request $request, string $id) {
 
         $request->validate([
-            'kategori_kode'    => 'required|max:3|unique:m_kategori,kategori_kode',
+            'kategori_kode'    => 'required|min:3|unique:m_kategori,kategori_kode',
             'kategori_nama'    => 'required|string'
         ]);
 
