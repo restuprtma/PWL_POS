21fc0853cacf4aba1ca103a5a6f13407be86fde3
restuprtma <restu.prtma05@gmail.com>
2025-03-11 21:46:44 +0700

    Implementasikan web layout dan datatables pada table Supplier

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -2,9 +2,152 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SupplierModel;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class SupplierController extends Controller
 {
-    //
+    public function index() {
+        $supplier = SupplierModel::all();
+
+        $breadcrumb = (object) [
+            'title' => 'Daftar Supplier',
+            'list'  => ['Home', 'Supplier']
+        ];
+
+        $page = (object) [
+            'title' => 'Daftar Supplier yang terdaftar dalam sistem'
+        ];
+
+        $activeMenu = 'supplier';
+
+        return view('supplier.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'supplier' => $supplier, 'activeMenu' => $activeMenu]);
+    }
+
+    public function list(Request $request) {
+        $supplier = SupplierModel::select('supplier_id', 'supplier_kode', 'supplier_nama', 'supplier_alamat');
+
+        //filter data supplier
+        if($request->supplier_id){
+            $supplier->where('supplier_id', $request->supplier_id);
+        }
+    
+        return DataTables::of($supplier)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($supplier) {
+                $btn = '<a href="'.url('/supplier/' .$supplier->supplier_id).'" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="'.url('/supplier/' .$supplier->supplier_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="'.url('/supplier/'.$supplier->supplier_id).'">'
+                    . csrf_field() . method_field('DELETE').
+                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah anda yakin menghapus data ini?\');">Hapus</button></form>';
+                return $btn; 
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
+
+    public function create()
+    {
+        $breadcrumb = (object) [
+            'title' => 'Tambah Supplier',
+            'list' => ['Home', 'Supplier', 'Tambah']
+        ];
+
+        $page = (object) [
+            'title' => 'Tambah supplier baru'
+        ];
+
+        $supplier = SupplierModel::all(); // ambil data supplier untuk ditampilkan di form
+        $activeMenu = 'supplier'; // set menu yang sedang aktif
+
+        return view('supplier.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'supplier' => $supplier, 'activeMenu' => $activeMenu]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            // suppliername harus diisi, berupa string, minimal 3 karakter, dan bernilai unik di tabel m_supplier kolom suppliername
+            'supplier_kode' => 'required|string|min:3|unique:m_supplier,supplier_kode',
+            'supplier_nama' => 'required|string|max:100', // nama harus diisi, berupa string, dan maksimal 100 karakter
+            'supplier_alamat' => 'required|string|max:225'
+        ]);
+
+        SupplierModel::create([
+            'supplier_kode' => $request->supplier_kode,
+            'supplier_nama' => $request->supplier_nama,
+            'supplier_alamat' => $request->supplier_alamat
+        ]);
+
+        return redirect('/supplier')->with('success', 'Data supplier berhasil disimpan');
+    }
+
+    public function show(string $id) {
+        $supplier = SupplierModel::find($id);
+
+        $breadcrumb = (object) [
+            'title' => 'Detail supplier',
+            'list'  => ['Home', 'supplier', 'Detail']
+        ];
+
+        $page = (object) [
+            'title' => 'Detail supplier'
+        ];
+        
+        $activeMenu = 'supplier';
+
+        return view('supplier.show', ['breadcrumb' => $breadcrumb, 'page' => $page, 'supplier' => $supplier, 'activeMenu' => $activeMenu]);
+    }
+
+    public function edit(string $id)
+    {
+        $supplier = SupplierModel::find($id);
+
+        $breadcrumb = (object) [
+            'title' => 'Edit supplier',
+            'list' => ['Home', 'Supplier', 'Edit']
+        ];
+
+        $page = (object) [
+            'title' => 'Edit Supplier'
+        ];
+
+        $activeMenu = 'supplier'; // set menu yang sedang aktif
+
+        return view('supplier.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'supplier' => $supplier, 'activeMenu' => $activeMenu]);
+    }
+
+    // Menyimpan perubahan data supplier
+    public function update(Request $request, string $id) {
+
+        $request->validate([
+            'supplier_kode'    => 'required|min:3|unique:m_supplier,supplier_kode',
+            'supplier_nama'    => 'required|string',
+            'supplier_alamat' => 'required|string|max:225'
+        ]);
+
+        SupplierModel::find($id)->update([
+            'supplier_kode'    => $request->supplier_kode,
+            'supplier_nama'    => $request->supplier_nama,
+            'supplier_alamat' => $request->supplier_alamat
+        ]);
+
+        return redirect('/supplier')->with('succes', 'Data supplier berhasil diubah');
+    }
+
+    public function destroy(string $id)
+    {
+        $check = SupplierModel::find($id); // untuk mengecek apakah data supplier dengan id yang dimaksud ada atau tidak
+        if (!$check) {
+            return redirect('/supplier')->with('error', 'Data supplier tidak ditemukan');
+        }
+
+        try {
+            SupplierModel::destroy($id); // Hapus data supplier
+            return redirect('/supplier')->with('success', 'Data supplier berhasil dihapus');
+        } catch (\Illuminate\Database\QueryException $e) {
+            // Jika terjadi error ketika menghapus data, redirect kembali ke halaman dengan membawa pesan error
+            return redirect('/supplier')->with('error', 'Data supplier gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
+        }
+    }
 }
--- app/Models/SupplierModel.php
+++ app/Models/SupplierModel.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SupplierModel extends Model
 {
-    use HasFactory;
+    protected $table = 'm_supplier';
+    protected $primaryKey = 'supplier_id';
+    protected $fillable = ['supplier_kode', 'supplier_nama', 'supplier_alamat'];
 }
